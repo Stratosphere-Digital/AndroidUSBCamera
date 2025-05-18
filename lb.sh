e8a2c35d6ff98d7f3d6ec621ea781d581760f034
ahcurrier <andrew@stratosphere.digital>
2025-05-18 09:37:41 -0400

    Remove script

--- lb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home
-export PATH="$JAVA_HOME/bin:$PATH"
-echo $JAVA_HOME
-echo $PATH
-java --version
-# cursor .
