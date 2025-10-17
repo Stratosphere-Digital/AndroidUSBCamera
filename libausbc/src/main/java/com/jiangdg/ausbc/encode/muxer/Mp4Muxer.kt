cf8d554777d72efa37950bb18fb52e2267264101
wesfolz <wesleyfolz@gmail.com>
2025-10-17 09:42:41 -0700

    fixed video recording issues

--- libausbc/src/main/java/com/jiangdg/ausbc/encode/muxer/Mp4Muxer.kt
+++ libausbc/src/main/java/com/jiangdg/ausbc/encode/muxer/Mp4Muxer.kt
@@ -258,7 +258,8 @@ class Mp4Muxer(
     private fun getVideoContentValues(path: String): ContentValues {
         val file = File(path)
         val values = ContentValues()
-        values.put(MediaStore.Video.Media.DATA, path)
+        // Note: DATA field is deprecated and cannot be set on Android 10+ (API 29+)
+        // Use RELATIVE_PATH instead for modern Android versions
         values.put(MediaStore.Video.Media.DISPLAY_NAME, file.name)
         values.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4")
         values.put(MediaStore.Video.Media.SIZE, file.length())
